8be5996a737f812a02c3280d908f1dcef34d2855
oladejomubarak <oladejomubarakade@gmail.com>
2024-04-18 17:43:28 +0100

    commiting

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container_file.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer( [
+            Color.fromARGB(255, 74, 156, 15),
+            Color.fromARGB(255, 100, 56, 176),
+            Color.fromARGB(255, 176, 56, 56),
+          ],)),
+    ),
+  );
+}
