473175a9bc3104b1b1c434bde2284f9f80018578
oladejomubarak <oladejomubarakade@gmail.com>
2024-04-19 15:49:35 +0100

    made little changes to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
-import 'package:first_app/challenge02/gradient_template_file.dart';
+//import 'package:first_app/challenge02/gradient_template_file.dart';
+import 'package:first_app/solution12/start_screen.dart';
 import 'package:flutter/material.dart';
 //import 'package:first_app/gradient_container_file.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+     MaterialApp(
       home: Scaffold(
             //solution12
-            body: ,
+            body: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors:[
+                    Color.fromARGB(255, 78, 13, 151),
+                    Color.fromARGB(255, 107, 15, 168),
+                  ], ),),
+              child: const StartScreen(),
+              
+              ),
          //challenge
         // body: GradientTemplate([
         // Color.fromARGB(255, 120, 49, 244),
